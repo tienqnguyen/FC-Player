b76ac900036a09a9d594329ce2d273b329c06938
tienqnguyen <menrichky@gmail.com>
2026-06-05 15:55:21 -0400

    chore: cleanup dev scripts and add favicon
    Remove obsolete data analysis scripts and add a project favicon to improve professional presentation.

--- test_metadata.ts
+++
@@ -1,6 +0,0 @@
-const url = "http://localhost:3000/api/metadata?url=https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-
-fetch(url).then(r => {
-  console.log('Status', r.status, r.headers.get('content-type'));
-  return r.text();
-}).then(console.log).catch(console.error);
